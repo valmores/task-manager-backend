1114dfb6407ba151e0b133ed29bc8b67e44026fe
valmores <johnericvalmores@gmail.com>
2026-05-03 20:26:36 +0800

    feat: add first_name and last_name to REQUIRED_FIELDS in CustomUser model

--- users/models.py
+++ users/models.py
@@ -44,7 +44,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['first_name', 'last_name']
 
     def __str__(self):
         return self.email
